db8c4f25782db4569ef69f8cea38cb2b36e9faa5
root <root@mopbzp174114.fr.dst.ibm.com>
2020-02-21 06:37:31 -0500

    new

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+var apm = require('elastic-apm-node').start({
+  // Override service name from package.json
+  // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+	serviceName: 'server'
+  // Use if APM Server requires a token
+  //     secretToken: '',
+  // Set custom APM Server URL (default: http://localhost:8200)
+  //    serverUrl: ''
+
+})
+
+
+
 var mysql = require('mysql');
 
 process.on('uncaughtException', (err) => {
